c152bd7ab2b9c8f68843fb5988246028dca2446d
DamascenoV <victor.c.damasceno@gmail.com>
2026-08-14 13:08:55 +0100

    Chapter 3.5 Evaluator - Integer

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,46 @@
+package evaluator
+
+import (
+	"github.com/damascenov/monkey_interpreter/lexer"
+	"github.com/damascenov/monkey_interpreter/object"
+	"github.com/damascenov/monkey_interpreter/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"4", 4},
+		{"7", 7},
+		{"24", 24},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+    program := p.ParseProgram()
+
+    return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+    result, ok := obj.(*object.Integer)
+    if !ok {
+        t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+        return false
+    }
+    if result.Value != expected {
+        t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+        return false
+    }
+
+    return true
+}
