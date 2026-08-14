efd48751544cb0c6a17fc3c4e550495708ad0226
DamascenoV <victor.c.damasceno@gmail.com>
2026-08-14 13:15:14 +0100

    Chapter 3.5 Evaluator - Boolean and REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/damascenov/monkey_interpreter/lexer"
 	"github.com/damascenov/monkey_interpreter/parser"
+	"github.com/damascenov/monkey_interpreter/evaluator"
 	"io"
 )
 
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
     }
 }
 
